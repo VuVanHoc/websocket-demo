5480372684e3313b87d811ccff334a33bb4144ec
VuVanHoc <hocvanvu1999@gmail.com>
2019-07-26 22:24:18 +0700

    create sample, add file config websocket

---
+++ src/main/java/com/itleadpro/training/websocket/model/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.itleadpro.training.websocket.model;
+
+import lombok.Data;
+
+@Data
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+
+}
